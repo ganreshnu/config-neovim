1ddb2c5100878f557bfdb57abbef8cd5ec662b6c
Ganreshnu <ganreshnu@gmail.com>
2023-10-10 13:24:12 -0500

    revising my config

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,7 +1,6 @@
-
 return {
-'ggandor/leap.nvim',
-config = function()
-	require('leap').add_default_mappings()
+	'ggandor/leap.nvim',
+	config = function()
+		require('leap').add_default_mappings()
 	end
 }
