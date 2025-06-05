f5b44a39de9462e1fcae840964875119b2b522ef
Ganreshnu <ganreshnu@gmail.com>
2025-06-05 06:18:46 -0500

    didn't need that last commit

--- init.lua
+++ init.lua
@@ -290,14 +290,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 })
 
-vim.api.nvim_create_autocmd("TermOpen", {
-	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-	callback = function()
-		vim.opt.number = false
-		vim.opt.relativenumber = false
-	end,
-})
-
 -- setup our basic keymaps
 require("keymaps").basic()
 
